cb0c042eca7bb439cba8e4478a6f8ccc462620fa
alexst <stolbov_alexey@mail.ru>
2018-01-31 21:32:45 +0300

    5.1.3. Создать итератор простые числа. [#151]

---
+++ Iterator/src/main/java/ru/astolbov/PrimeIterator.java
@@ -0,0 +1,74 @@
+package ru.astolbov;
+
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+/**
+ * Итератор для массива int[], по простым числам.
+ */
+public class PrimeIterator implements Iterator {
+
+    private int[] inputNumbers;
+    /**
+     * Pointer
+     */
+    private int pos = 0;
+
+    /**
+     * Constructor.
+     * @param inputNumbers
+     */
+    public PrimeIterator(int[] inputNumbers) {
+        this.inputNumbers = inputNumbers;
+        setPosToNextPrimeNumber();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return pos < inputNumbers.length;
+    }
+
+    @Override
+    public Object next() {
+        if (!hasNext()) {
+            throw new NoSuchElementException();
+        }
+        int res = inputNumbers[pos++];
+        setPosToNextPrimeNumber();
+        return res;
+    }
+
+    /**
+     * Move pointer to next prime number.
+     */
+    private void setPosToNextPrimeNumber() {
+        while (pos < inputNumbers.length && !numberIsPrime(inputNumbers[pos])) {
+            pos++;
+        }
+    }
+
+    /**
+     * Determines that this number is prime.
+     * @param number
+     * @return - true if this number is prime, otherwise false.
+     */
+    private boolean numberIsPrime(int number) {
+        boolean res = true;
+        if (number > 1) {
+            if (number > 2) {
+                if (number % 2 == 0) {
+                    res = false;
+                } else {
+                    for (int i = 2; i * i < number; i++) {
+                        if (number % i == 0) {
+                            res = false;
+                        }
+                    }
+                }
+            }
+        } else {
+            res = false;
+        }
+        return res;
+    }
+}
