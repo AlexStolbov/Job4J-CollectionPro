4ec8268c268156df196705267c93bf94a565a971
alexst <stolbov_alexey@mail.ru>
2018-02-01 21:08:00 +0300

    5.1.4. Создать convert(Iterator<Iterator>) [#152]

---
+++ Iterator/src/main/java/ru/astolbov/Converter.java
@@ -0,0 +1,44 @@
+package ru.astolbov;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.NoSuchElementException;
+
+public class Converter {
+
+    Iterator<Integer> convert(Iterator<Iterator<Integer>> it) {
+
+        Iterator<Integer> resIterator = (new ArrayList<Integer>()).iterator();
+
+        if (it.hasNext()) {
+            resIterator = new Iterator<Integer>() {
+                Iterator<Integer> pos = it.next();
+
+                @Override
+                public boolean hasNext() {
+                    return pos.hasNext();
+                }
+
+                @Override
+                public Integer next() {
+                    if (!hasNext()) {
+                        throw new NoSuchElementException();
+                    }
+                    Integer next = pos.next();
+                    moveToNextPosition();
+                    return next;
+                }
+
+                private void moveToNextPosition() {
+                    if (!pos.hasNext()) {
+                        if (it.hasNext()) {
+                            pos = it.next();
+                        }
+                    }
+                }
+
+            };
+        }
+        return resIterator;
+    }
+}
